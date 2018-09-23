5c93ba8a8e00d936211b20af7789aae0e2722a36
Player Thong <playerthong@gmail.com>
2018-09-24 01:04:54 +0700

    add simple data response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  res.send({"responseId":req.body.responseId});
+  var data={
+    "payload": {
+      "google": {
+        "expectUserResponse": true,
+        "richResponse": {
+          "items": [
+            {
+              "simpleResponse": {
+                "textToSpeech": "this is a simple response"
+              }
+            }
+          ]
+        }
+      }
+    }
+  };
+  
+  res.send(data);
 });
 
 module.exports = router;
